eb2ae0dfb85401cf3f7c8327fe60e800072e1426
aymansohag <aymansohag.contact@gmail.com>
2023-01-27 00:32:06 +0600

    route file foldering

--- routes/api.php
+++ routes/api.php
@@ -4,27 +4,14 @@ use App\Http\Controllers\Api\User\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-
 Route::controller(AuthController::class)->group(function () {
     Route::post('/login', 'login');
     Route::post('/register', 'register');    
 });
 Route::middleware('auth:sanctum')->group(function (){
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::get('/user', [AuthController::class, 'user']);
+
+    Route::controller(AuthController::class)->group(function(){
+        Route::post('/logout', 'logout');
+        Route::get('/user', 'user');
+    });
 });
